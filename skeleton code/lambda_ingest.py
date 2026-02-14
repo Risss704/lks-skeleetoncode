4b382395f5f4a724884b8692fd22163fd8beef58
Risss704 <risqy704@gmail.com>
2026-02-14 07:24:46 +0700

    Refactor lambda_ingest.py for improved message handling

--- skeleton code/lambda_ingest.py
+++ skeleton code/lambda_ingest.py
@@ -1,57 +1,34 @@
 import json
-import os
 import boto3
-import uuid
+import os
 
-# Client SQS
 sqs = boto3.client('sqs')
-
-# Ambil URL Queue dari Environment Variable
-SQS_URL = os.environ.get('SQS_URL')
-
+QUEUE_URL = os.environ['SQS_URL']
 
 def lambda_handler(event, context):
 
-    try:
-        # Ambil data dari request ALB
-        if "body" in event and event["body"]:
-            body = json.loads(event["body"])
-        else:
+    body = {}
+
+    if 'body' in event and event['body']:
+        try:
+            body = json.loads(event['body'])
+        except:
             body = {}
 
-        # Buat ID unik order
-        order_id = str(uuid.uuid4())
+    sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(body)
+    )
 
-        # Data pesanan
-        order_data = {
-            "id": order_id,
-            "name": body.get("name", "unknown"),
-            "product": body.get("product", "unknown"),
-            "quantity": body.get("quantity", 1)
-        }
-
-        # Kirim ke SQS
-        sqs.send_message(
-            QueueUrl=SQS_URL,
-            MessageBody=json.dumps(order_data)
-        )
-
-        # Response ke client
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({
-                "message": "Order berhasil diterima",
-                "order_id": order_id
-            })
-        }
-
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({
-                "error": str(e)
-            })
-        }
+    return {
+        "statusCode": 200,
+        "statusDescription": "200 OK",
+        "isBase64Encoded": False,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "status": "success",
+            "message": "Order masuk ke queue"
+        })
+    }
